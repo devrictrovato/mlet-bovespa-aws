d93a1af27fedaf6969736b927ab6f6b544f5ec34
Ricardo O. Trovato <devrictrovato@gmail.com>
2025-08-01 22:04:07 -0300

    Update crawler-bovespa.py

--- crawler-bovespa.py
+++ crawler-bovespa.py
@@ -1,3 +1,4 @@
+import datetime
 import pandas as pd
 from scripts.conversor import convert_df_to_parquet_bytes
 from scripts.get_data import get_html_with_selenium, parse_table_with_bs4
@@ -7,10 +8,10 @@ from scripts.upload_file import upload_parquet_to_s3
 # 🚀 Execução
 if __name__ == "__main__":
     url = "https://sistemaswebb3-listados.b3.com.br/indexPage/day/IBOV?language=pt-br"
-    html = get_html_with_selenium(url, True)
+    html = get_html_with_selenium(url, False)
     headers, rows = parse_table_with_bs4(html)
     df = pd.DataFrame(rows[:-2], columns=headers)
-    print(df)
+    df['data'] = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     parquet_bytes = convert_df_to_parquet_bytes(df)
     upload_parquet_to_s3(parquet_bytes)
 
